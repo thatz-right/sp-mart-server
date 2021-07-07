51862c07aa43ac3b8c7d720a8311e66a7f1ef043
Songwonseok <dnjstjr0517@gmail.com>
2021-07-07 17:19:14 +0900

    refactor: id 타입 변경 및 생성자 수정
    - null 값을 할당할 수 있도록 long -> Long 으로 변경
    - 연관관계 편의를 위한 로직 추가

--- src/main/java/com/spmart/server/order/domain/OrderProduct.java
+++ src/main/java/com/spmart/server/order/domain/OrderProduct.java
@@ -1,6 +1,7 @@
 package com.spmart.server.order.domain;
 
 import com.spmart.server.product.domain.Product;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,28 +13,31 @@ import javax.validation.constraints.Min;
 @Getter
 @Entity
 public class OrderProduct {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    private Product product;
+	private int orderPrice;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id")
+	private Product product;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    private Order order;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
 
-    private int orderPrice;
+	@Min(value = 0)
+	private int count;
 
-    @Min(value = 0)
-    private int count;
+	@Builder
+	public OrderProduct(Long id, Product product, Order order, int count) {
+		this.id = id;
+		this.product = product;
 
-    @Builder
-    public OrderProduct(Product product, Order order, int count) {
-        this.product = product;
-        this.order = order;
-        this.count = count;
-        this.orderPrice = product.getDiscountPrice() * count;
-    }
+		this.order = order;
+		order.getOrderProducts().add(this);
+
+		this.count = count;
+		this.orderPrice = product.getDiscountPrice() * count;
+	}
 }
