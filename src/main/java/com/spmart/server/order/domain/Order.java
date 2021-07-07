a4f17826e52fc9b118d3a43e6d67e97a94804503
Songwonseok <dnjstjr0517@gmail.com>
2021-07-07 17:18:47 +0900

    refactor: id 타입 변경
    - null 값을 할당할 수 있도록 long -> Long 으로 변경

--- src/main/java/com/spmart/server/order/domain/Order.java
+++ src/main/java/com/spmart/server/order/domain/Order.java
@@ -2,11 +2,13 @@ package com.spmart.server.order.domain;
 
 import com.spmart.server.auth.domain.User;
 import com.spmart.server.common.BaseTimeEntity;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,24 +17,25 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 public class Order extends BaseTimeEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderProduct> orderProduct = new ArrayList<>();
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<OrderProduct> orderProducts = new ArrayList<>();
 
-    @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
+	@Enumerated(EnumType.STRING)
+	private OrderStatus orderStatus;
 
-    @Builder
-    public Order(User user, List<OrderProduct> orderProduct, OrderStatus orderStatus) {
-        this.user = user;
-        this.orderProduct = orderProduct;
-        this.orderStatus = orderStatus != null ? orderStatus : OrderStatus.PAYED;
-    }
+	@Builder
+	public Order(Long id, User user, List<OrderProduct> orderProducts, OrderStatus orderStatus) {
+		this.id = id;
+		this.user = user;
+		this.orderProducts = orderProducts;
+		this.orderStatus = orderStatus != null ? orderStatus : OrderStatus.PAYED;
+	}
 }
